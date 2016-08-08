fffe260465f99bca68122b8c6ad88db84459a941
Tony Merritt <Klavar@Meninx.home>
2016-08-09 00:27:57 +0100

    Initial Commit

---
+++ Fight 2 player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fight 2 player
+//
+//  Created by Tony Merritt on 09/08/2016.
+//  Copyright © 2016 Tony Merritt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
