b1b086d4ef92054d794330e0f5dc135efc63cea4
Tony Merritt <Klavar@Meninx.home>
2016-08-10 22:48:24 +0100

    Added more code and a death scene with delay before restert, as well as music and sound.

--- Fight 2 player/ViewController.swift
+++ Fight 2 player/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
     
     //Setting my outlets showing the parts are there to be used.
     @IBOutlet weak var startGame: UIButton!
@@ -20,16 +22,106 @@ class ViewController: UIViewController {
     @IBOutlet weak var attacklabelTwo: UILabel!
     @IBOutlet weak var enemyImage: UIImageView!
     @IBOutlet weak var playerImage: UIImageView!
+   
+    @IBOutlet weak var playerHead: UIImageView!
+    
+    @IBOutlet weak var enemyHead: UIImageView!
+
+    //Setting the Sound variables
+    var plSpear = AVAudioPlayer()
+    var plClub = AVAudioPlayer()
+    var plmusic = AVAudioPlayer()
+    var plDeath = AVAudioPlayer()
+    
+    //Setting the function of playing different sounds together
+    func playMultipleSound() {
+        playSpear()
+        playClub()
+        playMusic()
+        playDeath()
+    }
+    //Setting the functions of the Music, stab hit and death with the properties.
+    func playMusic() {
+        let soundUrl = NSBundle.mainBundle().URLForResource("Music", withExtension: "wav")!
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            plmusic = try AVAudioPlayer(contentsOfURL: soundUrl)
+            plmusic.volume = 0.1
+            plmusic.numberOfLoops = 3
+            plmusic.prepareToPlay()
+            plmusic.play()
+        } catch _ {
+            return print("sound file not found")
+        }
+    }
+    
+    
+    func playSpear() {
+        let soundUrl = NSBundle.mainBundle().URLForResource("Spear", withExtension: "wav")!
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            plSpear = try AVAudioPlayer(contentsOfURL: soundUrl)
+            plSpear.numberOfLoops = 0
+            plSpear.prepareToPlay()
+            plSpear.play()
+        } catch _ {
+            return print("sound file not found")
+        }
+    }
+    
+    func playClub() {
+        let soundUrl = NSBundle.mainBundle().URLForResource("Club", withExtension: "wav")!
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            plClub = try AVAudioPlayer(contentsOfURL: soundUrl)
+            plClub.numberOfLoops = 0
+            plClub.prepareToPlay()
+            plClub.play()
+        } catch _ {
+            return print("sound file not found")
+        }
+    }
+    
+    func playDeath() {
+        let soundUrl = NSBundle.mainBundle().URLForResource("Death", withExtension: "wav")!
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            plDeath = try AVAudioPlayer(contentsOfURL: soundUrl)
+            plDeath.numberOfLoops = 0
+            plDeath.prepareToPlay()
+            plDeath.play()
+        } catch _ {
+            return print("sound file not found")
+        }
+    }
+    
+    
+    
     
     //setting the Player and Enemy variables.
     var player: Player!
     var enemy: Enemy!
     
+    
     //Start of Main.
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+
         //Making the Player and Enemy and Buttons hidden, but showing the start button.
+        playMusic()
+        
         startGame.hidden = false
         playerImage.hidden = true
         enemyImage.hidden = true
@@ -39,6 +131,8 @@ class ViewController: UIViewController {
         attackTwo.hidden = true
         attackLabelOne.hidden = true
         attacklabelTwo.hidden = true
+        enemyHead.hidden = true
+        playerHead.hidden = true
         
         
         //Setting the players name and random Health and attack.
@@ -50,33 +144,64 @@ class ViewController: UIViewController {
     
     //Setting the function of attack one btton.
     @IBAction func onAttackOnePressed(sender: AnyObject){
-    
+
+        playClub()
+        
+        attacklabelTwo.hidden = true
+        attackTwo.hidden = true
+        
         if enemy.attemptAttack(player.attackPower) {
         
             infoText.text = "Meninx attacked \(enemy.name) for \(player.attackPower) HP"
         }else{
             infoText.text = "Attack missed"
-        }
+    }
+        attackLabelOne.hidden = true
+        attackOne.hidden = true
+        attacklabelTwo.hidden = false
+        attackTwo.hidden = false
+        
         if !enemy.isAlive {
+            playDeath()
+            enemyHead.hidden = false
+            enemyImage.hidden = true
+            attackTwo.hidden = true
+            attacklabelTwo.hidden = true
             infoText.text = "Meninx Killed \(enemy.name)"
-            viewDidLoad()
+            NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: #selector(ViewController.viewDidLoad), userInfo: nil, repeats: false)
+            
         }
+        
     }
     
     //Setting the function of attack two button.
     @IBAction func onAttackTwoPressed(sender: AnyObject) {
-        
+       
+        playSpear()
+
         if player.attemptAttack(enemy.attackPower) {
             
             infoText.text = "Skitz attacked \(player.name) for \(enemy.attackPower) HP"
         }else{
             infoText.text = "Attack missed"
-        }
-        if !player.isAlive {
-            infoText.text = "Skitz Killed \(player.name)"
-            viewDidLoad()
-        }
     }
+        attacklabelTwo.hidden = true
+        attackTwo.hidden = true
+        attackLabelOne.hidden = false
+        attackOne.hidden = false
+        
+        if !player.isAlive {
+            playDeath()
+            playerHead.hidden = false
+            playerImage.hidden = true
+            attackOne.hidden = true
+            attackLabelOne.hidden = true
+            infoText.text = "Skitz Killed \(player.name)"
+            NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: #selector(ViewController.viewDidLoad), userInfo: nil, repeats: false)
+    }
+     
+}
+
     
     //setting the items of the game as un-hidden when pressing start.
     @IBAction func onStartPressed(sender: AnyObject) {
@@ -89,9 +214,9 @@ class ViewController: UIViewController {
         attackTwo.hidden = false
         attackLabelOne.hidden = false
         attacklabelTwo.hidden = false
-        }
+ 
+        
     }
-
 
 //Setting the random numbers for the health and the attack
 func randomNumber(range: Range<Int> = 10...20) -> Int {
@@ -100,5 +225,5 @@ func randomNumber(range: Range<Int> = 10...20) -> Int {
     return Int(arc4random_uniform(UInt32(max - min))) + min
     
     
-        
+    }
 }
